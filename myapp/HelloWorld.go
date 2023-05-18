0415710bb5755a98db90bfcc9f09f36d83f65c4a
Chamith <chamith.eos@gmail.com>
2023-05-18 06:41:59 +0530

    Committed on 5/18/2023

---
+++ myapp/HelloWorld.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	_ "github.com/labstack/echo/v4"
+	"net/http"
+	_ "net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	})
+	e.Logger.Fatal(e.Start(":123"))
+}
