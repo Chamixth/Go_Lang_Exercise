1507ff025424362f945a27c6df3edce8b8b3915c
Chamith <chamith.eos@gmail.com>
2023-05-19 10:51:13 +0530

    committed on 5/19/23

--- myapp/go.mod
+++ myapp/go.mod
@@ -1,5 +1,3 @@
 module myapp
 
 go 1.20
-
-require github.com/briandowns/openweathermap v0.19.0
--- myapp/server.go
+++ myapp/server.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	_ "encoding/json"
+	"fmt"
 	"io/ioutil"
+	"net/http"
 	_ "net/http"
+	"strings"
 	_ "strings"
 )
 
@@ -13,9 +16,11 @@ type apiConfigData struct {
 }
 
 type weatherData struct {
-	Name string `json:"Name"`
+	Name string `json:"name"`
 	Main struct {
-		Kelvin float64 `json:"Temp"`
+		Temp     float64 `json:"temp"`
+		Pressure float64 `json:"pressure"`
+		Humidity int     `json:"humidity"`
 	} `json:"main"`
 }
 
@@ -33,4 +38,59 @@ func loadApiConfig(filename string) (apiConfigData, error) {
 	}
 
 	return c, nil
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from go!\n"))
+}
+
+func query(city string) (weatherData, error) {
+	apiConfig, err := loadApiConfig(".apiConfig")
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	url := "http://api.openweathermap.org/data/2.5/weather?APPID=" + apiConfig.OpenWeatherMapApiKey + "&q=" + city
+	fmt.Println("API URL:", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return weatherData{}, err
+	}
+	fmt.Println("API Response:", string(body))
+
+	var d weatherData
+	if err := json.Unmarshal(body, &d); err != nil {
+		fmt.Println("JSON decoding error:", err)
+		return weatherData{}, err
+	}
+	d.Main.Temp = d.Main.Temp - 273.15
+	fmt.Println("Decoded weather data:", d)
+
+	return d, nil
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/weather/",
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := query(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json; charset=utf8.")
+			json.NewEncoder(w).Encode(data)
+		})
+
+	http.ListenAndServe(":8080", nil)
 }
